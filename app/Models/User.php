e856f50fdcfaa7e6c527ffa749fd7f7dd9eb4cfc
helloasir <helloasir@gmail.com>
2022-12-01 06:04:08 +0400

    Build Chirper with Blade

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +42,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function chirps()
+    {
+        return $this->hasMany(Chirp::class);
+    }
 }
